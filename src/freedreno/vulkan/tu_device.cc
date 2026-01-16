c14e4e3df7010fafd2cbda324ffaf15fff5d2ff2
whitebelyash <whbexiumwork@gmail.com>
2026-01-17 01:17:14 +0400

    HACK: tu: add adrenotools-specific driver versioning & git commit hash to device name
    This helps finding out what driver version is used. Obviously not for upstream
    
    Signed-off-by: whitebelyash <whbexiumwork@gmail.com>

--- src/freedreno/vulkan/tu_device.cc
+++ src/freedreno/vulkan/tu_device.cc
@@ -50,6 +50,9 @@
 #include "wsi_common.h"
 #endif
 
+#include "git_sha1.h"
+#include "tu_version.h"
+
 #if DETECT_OS_ANDROID
 #include <vndk/hardware_buffer.h>
 #endif
@@ -1316,9 +1319,10 @@ tu_get_properties(struct tu_physical_device *pdevice,
    props->sparseResidencyAlignedMipSize = false;
    props->sparseResidencyNonResidentStrict = true;
 
-   snprintf(props->deviceName, sizeof(props->deviceName), "%s",
-            (strlen(pdevice->instance->drirc.debug.force_vk_devicename) > 0) ?
-            pdevice->instance->drirc.debug.force_vk_devicename : pdevice->name);
+   char devname[128];
+   strcpy(devname, pdevice->name);
+   strcat(devname, MESA_GIT_SHA1 "/" TUGEN8_DRV_VERSION);
+   strcpy(props->deviceName, devname);
    memcpy(props->pipelineCacheUUID, pdevice->cache_uuid, VK_UUID_SIZE);
 
    if (TU_DEBUG(DECK_EMU)) {
