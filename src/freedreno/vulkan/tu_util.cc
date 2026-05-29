969ef1321d0584573679b3252cde6e9a0ac1ae8d
DVD <disk_dvd@outlook.com>
2026-05-29 19:36:03 +0300

    Freedreno/vulcan: Add deck_emu

--- src/freedreno/vulkan/tu_util.cc
+++ src/freedreno/vulkan/tu_util.cc
@@ -55,7 +55,8 @@ static const struct debug_control tu_debug_options[] = {
    { "comm", TU_DEBUG_COMM },
    { "nofdm", TU_DEBUG_NOFDM },
    { "nocb", TU_DEBUG_NO_CONCURRENT_BINNING },
-   { "forcecb", TU_DEBUG_FORCE_CONCURRENT_BINNING },
+   { "forcecb", TU_DEBUG_FORCE_CONCURRENT_BINNING },  
+   {"deck_emu", TU_DEBUG_DECK_EMU },
    { NULL, 0 }
 };
 
