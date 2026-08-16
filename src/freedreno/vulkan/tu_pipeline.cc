66a9490d69d661baebc8cd80cff0a60959ad1aff
DVD <disk_dvd@outlook.com>
2026-08-16 23:09:24 +0500

    Fix error compiler
    I am sleep

--- src/freedreno/vulkan/tu_pipeline.cc
+++ src/freedreno/vulkan/tu_pipeline.cc
@@ -1745,7 +1745,7 @@ tu_pipeline_builder_compile_shaders(struct tu_pipeline_builder *builder,
 
    const uint64_t chip_id = builder->device->physical_device->dev_id.chip_id;
    const bool is_a810 = chip_id == 0x44010000ull;
-   const bool ia_a812 = chip_id == 0x44010200ull;
+   const bool is_a812 = chip_id == 0x44010200ull;
    const bool is_a825 = chip_id == 0x44030000ull;
    const bool is_a829 = chip_id == 0x44030A20ull;
    const bool is_a830 = chip_id == 0xffff44050000 || 0x44050001;
